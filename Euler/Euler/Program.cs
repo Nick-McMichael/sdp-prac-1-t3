6193cbd9e5a8bf656f16cc64c72d7291be5e1d9f
Matt <matt2hagemann@gmail.com>
2019-02-15 10:24:47 +0200

    Matts commit

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Euler
 {
     class Program
     {
+
+        
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            Console.WriteLine($"Problem 1: {Problem1.solve()}");
+        }
+    }
+
+    class Problem1
+    {
+        public static int solve()
+        {
+            int ans = 0;
+            List<int> list = new List<int>();
+
+            for (int x = 0; x < 1000; x++)
+            {
+                if (x % 3 == 0 || x % 5 == 0)
+                {
+                    list.Add(x);
+                }
+            }
+
+            for (int y = 0; y < list.Count; y++)
+            {
+                ans += list[y];
+            }
+
+            return ans;
         }
     }
 }
