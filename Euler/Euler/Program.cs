c2b78ffbe74464a99de8183bd0e30fa7b79e533f
andrewnel <andrew.nel.an@gmail.com>
2019-02-15 08:04:46 +0200

    My solution to Q13 SumsOfSquares

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -6,7 +6,23 @@ namespace Euler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"The solution to Q13 is {SolveQ13Euler.SolveQ13()}\n\n");
+        }
+    }
+
+    class SolveQ13Euler
+    {
+        //Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+        public static string SolveQ13()
+        {
+            int sumOfSquares = 0; int squareOfSums=0;
+            for (int i = 1; i <= 100; i++)
+            {
+                sumOfSquares += i * i;
+                squareOfSums += i;
+            }
+            squareOfSums = squareOfSums * squareOfSums;
+            return Convert.ToString(squareOfSums- sumOfSquares);
         }
     }
 }
